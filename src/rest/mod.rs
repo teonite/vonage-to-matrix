ec0ff2919d1f655dd58bcf059544f377ffe3e999
Kamil Chudy <kchudy@teonite.com>
2023-07-14 16:46:18 +0200

    Added inbound call support

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -21,13 +21,44 @@ pub struct VonageInboundMessage {
     pub message_timestamp: String,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct VonageInboundCall {
+    pub call_id: String,
+    pub status: String,
+    #[serde(alias = "call-direction")]
+    pub call_direction: String,
+    pub to: String,
+    pub from: String,
+    #[serde(alias = "call-request")]
+    pub call_request: String,
+    #[serde(alias = "network-code")]
+    pub network_code: String,
+    #[serde(alias = "call-duration")]
+    pub call_duration: String,
+    #[serde(alias = "call-start")]
+    pub call_start: String,
+    #[serde(alias = "call-end")]
+    pub call_end: String,
+    #[serde(alias = "call-price")]
+    pub call_price: String,
+    #[serde(alias = "call-rate")]
+    pub call_rate: String,
+}
+
 #[post("/api/inbound-message")]
 async fn handle_inbound_message(web::Form(form): web::Form<VonageInboundMessage>) -> HttpResponse {
     info!("Received message {}", form.text);
     HttpResponse::Ok().finish()
 }
 
+#[post("/api/inbound-call")]
+async fn handle_inbound_call(web::Form(form): web::Form<VonageInboundCall>) -> HttpResponse {
+    info!("Received call from {}", form.from);
+    HttpResponse::Ok().finish()
+}
+
 pub fn config_service(config: &mut ServiceConfig) {
     config
-        .service(handle_inbound_message);
+        .service(handle_inbound_message)
+        .service(handle_inbound_call);
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,5 @@
 use actix_web::{test, App};
-use vonage_to_matrix::rest::{config_service, VonageInboundMessage};
+use vonage_to_matrix::rest::{config_service, VonageInboundCall, VonageInboundMessage};
 
 
 #[actix_web::test]
@@ -22,6 +22,35 @@ async fn test_handle_inbound_message() {
     };
 
     let req = test::TestRequest::post().uri("/api/inbound-message").set_form(form).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
+}
+
+
+#[actix_web::test]
+async fn test_handle_inbound_call() {
+    let app = test::init_service(
+        App::new()
+            .configure(config_service),
+    )
+    .await;
+
+    let form = VonageInboundCall {
+        call_id: String::from("2adb66c0b8f71835ba2a570956063b2c"),
+        status: String::from("ok"),
+        call_direction: String::from("in"),
+        to: String::from("48111111111"),
+        from: String::from("48222222222"),
+        call_request: String::from("2023-07-14+14%3A27%3A23"),
+        network_code: String::from("26006"),
+        call_duration: String::from("3"),
+        call_start: String::from("2023-07-14+14%3A27%3A27"),
+        call_end: String::from("2023-07-14+14%3A27%3A30"),
+        call_price: String::from("0.00036000"),
+        call_rate: String::from("0.00720000"),
+    };
+
+    let req = test::TestRequest::post().uri("/api/inbound-call").set_form(form).to_request();
     let resp = test::call_service(&app, req).await;
     assert!(resp.status().is_success());
 }
